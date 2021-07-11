292a396f6470718f6f4a5077ee6cdfcf2b03d2af
Giovanny Piovesan <giowrp@gmail.com>
2021-07-11 00:07:51 -0300

    feat: add currency formats

--- lib/core/platform/currency_formats.dart
+++ lib/core/platform/currency_formats.dart
@@ -2,7 +2,7 @@ import 'package:intl/intl.dart';
 
 class CurrencyFormats {
   String formatCurrency(double amount) {
-    final formatter = NumberFormat.currency();
+    final formatter = NumberFormat.currency(symbol: 'USD ');
     return formatter.format(amount);
   }
 }
