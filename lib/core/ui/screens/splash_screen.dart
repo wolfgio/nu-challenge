b3a509a37ade45572356047f2388d51563915a93
Giovanny Piovesan <giowrp@gmail.com>
2021-07-12 16:57:45 -0300

    chore: increased timeout at splash_screen

--- lib/core/ui/screens/splash_screen.dart
+++ lib/core/ui/screens/splash_screen.dart
@@ -19,7 +19,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 2), () {
+    Future.delayed(Duration(seconds: 4), () {
       widget.appNavigator.pushAndReplace('/home');
     });
     super.initState();
